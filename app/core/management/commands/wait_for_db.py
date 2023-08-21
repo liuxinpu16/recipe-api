ecea087f102a0eafe260393035ce8da19be26cf1
Xinpu Liu <xinpuliu@m1mbp16.local>
2023-08-20 22:48:09 -0700

    configure db migration

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,23 @@
+"""
+Django command to wait for db to be available
+"""
+import time
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        self.stdout.write("waiting for database...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write("Ddatabase unavailable, waiting 1s to retry...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("dabase available !"))
