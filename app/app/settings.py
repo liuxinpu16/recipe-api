27e46424a7e9462d11c61e4968c7f62839afa4fa
Xinpu Liu <xinpuliu@m1mbp16.local>
2023-08-21 15:25:57 -0700

    django admin setup

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1!cbx97=5^o(xg4jig15nn6#aw(qa-(yxbn3s6t4_g(vsu#73l"
+SECRET_KEY = (
+    "django-insecure-1!cbx97=5^o(xg4jig15nn6#aw(qa-(yxbn3s6t4_g(vsu#73l"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,3 +129,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "core.User"
